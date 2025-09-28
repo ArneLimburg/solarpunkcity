33d26f25aba43455d52115ec79a53ebf78b39a2a
Arne Limburg <arne@apache.org>
2025-09-28 19:04:03 +0200

    feat: Initial game loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import "beercss/dist/cdn/beer.min.css";
-import { useCallback, useRef, useState, type FC } from "react";
+import { useCallback, useEffect, useRef, useState, type FC } from "react";
 import { Map } from "./Map";
 import { Menu } from "./Menu";
 import { GRID_RADIUS, type HexCoordinates } from "./board";
-import { BuildingTypes, type BuildingType, type Model } from "./game";
+import {
+  BuildingTypes,
+  type BuildingType,
+  type Model,
+} from "./gameDefinitions";
+import { build, calculateWeek, canBuild } from "./game";
+import { emptyResources } from "./resources";
 
 export const App: FC = () => {
+  const [currentPopulation, setCurrentPopulation] = useState(0);
+  const [currentResources, setCurrentResources] = useState({
+    ...emptyResources,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const { resources, population } = calculateWeek();
+      setCurrentResources({ ...resources });
+      setCurrentPopulation(population);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    console.log(`population: ${currentPopulation}`);
+  }, [currentPopulation]);
+
+  useEffect(() => {
+    for (const [key, value] of Object.entries(currentResources)) {
+      if (value > 0) {
+        console.log(`${key}: ${value}`);
+      }
+    }
+  }, [currentResources]);
+
   const mapRef = useRef<{
     addBuilding: (coords: HexCoordinates, model: Model) => void;
   } | null>(null);
@@ -20,8 +53,10 @@ export const App: FC = () => {
     setSelectedBuildingType((selectedBuildingType) => {
       if (
         selectedBuildingType &&
-        Math.abs(coords.q + coords.r) <= GRID_RADIUS
+        Math.abs(coords.q + coords.r) <= GRID_RADIUS &&
+        canBuild(selectedBuildingType, coords)
       ) {
+        build(selectedBuildingType, coords);
         mapRef.current?.addBuilding(
           coords,
           BuildingTypes[selectedBuildingType].model,
---
+++ src/gameDefinitions.ts
@@ -0,0 +1,124 @@
+import { emptyResources, type Resources } from "./resources";
+
+export type Model = string;
+export type BuildableCategory = {
+  name: string;
+  buildable: true;
+  icon: string;
+};
+export type NotBuildableCategory = {
+  name: string;
+  buildable: false;
+};
+export const BuildingCategories = {
+  FARM: {
+    name: "FARM",
+    buildable: true,
+    icon: "farm/farm-icon.png",
+  },
+  RESIDENTIAL: {
+    name: "RESIDENTIAL",
+    buildable: true,
+    icon: "residential/residential-icon.png",
+  },
+  FOREST: {
+    name: "FOREST",
+    buildable: false,
+  },
+} as const satisfies Record<string, BuildableCategory | NotBuildableCategory>;
+export type BuildingCategory = keyof typeof BuildingCategories;
+export const BuildingTypes = {
+  OrganicHomestead: {
+    category: "FARM",
+    icon: "farm/farm-icon.png",
+    model: "farm/farm.glb",
+  },
+  Farm: {
+    category: "FARM",
+    icon: "farm/farm-icon.png",
+    model: "farm/farm.glb",
+  },
+  AgroIndustrial: {
+    category: "FARM",
+    icon: "farm/farm-icon.png",
+    model: "farm/farm.glb",
+  },
+  AppartmentBuilding: {
+    category: "RESIDENTIAL",
+    icon: "residential/residential-icon.png",
+    model: "residential/residential.glb",
+  },
+  Forest: {
+    category: "FOREST",
+    icon: "forest.png",
+    model: "forest.glb",
+  },
+} as const satisfies Record<
+  string,
+  { category: BuildingCategory; icon: string; model: Model }
+>;
+export type BuildingType = keyof typeof BuildingTypes;
+export type Building = {
+  name: string;
+  type: BuildingType;
+  description: string;
+  size: number;
+  resident: number;
+  cost: Resources;
+  produces: Resources;
+  consumes: Resources;
+  recycling?: Resources;
+};
+export const BuildingDefinition: Record<BuildingType, Building> = {
+  OrganicHomestead: {
+    name: "Organic homestead",
+    type: "OrganicHomestead",
+    description: "Produces food for the residents",
+    size: 1,
+    resident: 3,
+    cost: { ...emptyResources, wood: 4 }, // 40 cu m
+    produces: { ...emptyResources, food: 3 },
+    consumes: { ...emptyResources, labor: 2 },
+  },
+  Farm: {
+    name: "Farm",
+    type: "Farm",
+    description: "Produces food for the residents",
+    size: 7,
+    resident: 2,
+    cost: { ...emptyResources, wood: 10 },
+    produces: { ...emptyResources, food: 20 },
+    consumes: { ...emptyResources, labor: 4 },
+  },
+  AgroIndustrial: {
+    name: "Agro-industrial Farm",
+    type: "AgroIndustrial",
+    description: "Advanced farming techniques to boost food production",
+    size: 19,
+    resident: 0,
+    cost: { ...emptyResources, wood: 20 },
+    produces: { ...emptyResources, food: 60 },
+    consumes: { ...emptyResources, labor: 8 },
+  },
+  Forest: {
+    name: "Forest",
+    type: "Forest",
+    description: "Provides wood for building and crafting",
+    size: 1,
+    resident: 0,
+    cost: { ...emptyResources }, // requires forestry
+    produces: { ...emptyResources, wood: 0.2 },
+    consumes: { ...emptyResources, labor: 1 }, // later water
+    recycling: { ...emptyResources, wood: 7 },
+  },
+  AppartmentBuilding: {
+    name: "Appartment Building",
+    type: "AppartmentBuilding",
+    description: "Building for people to live",
+    size: 1,
+    resident: 15,
+    cost: { ...emptyResources, wood: 10 },
+    produces: emptyResources,
+    consumes: { ...emptyResources }, // later water and energy
+  },
+};
