c51072acce165311b7a32380c39cacfd6d3d43fb
Arne Limburg <arne@apache.org>
2025-09-28 19:04:03 +0200

    feat: Select different buildings

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,95 +1,53 @@
 import { useState, type FC } from "react";
+import {
+  BuildingCategories,
+  BuildingTypes,
+  type BuildingCategory,
+  type BuildingType,
+} from "./game";
 
 export const Menu: FC<{
-  selectedIcon: number;
-  onSelect: (icon: number) => void;
-}> = ({ selectedIcon, onSelect }) => {
-  const [open, setOpen] = useState(0);
+  selected: BuildingType | undefined;
+  onSelect: (buildingType: BuildingType) => void;
+}> = ({ selected, onSelect }) => {
+  const [open, setOpen] = useState<BuildingCategory | undefined>();
   return (
     <header className="transparent">
       <nav className="top transparent">
         <div className="max"></div>
-        <div className="details">
-          <button
-            className={`border button extra circle ${selectedIcon !== 1 ? "large-elevate" : "small-elevate"}`}
-            onClick={() => setOpen(1)}
-          >
-            <img className="responsive" src="farm/farm-icon.png" />
-          </button>
-          {open === 1 && (
-            <nav className="list">
-              <a
-                className="item"
-                onClick={() => {
-                  setOpen(0);
-                  onSelect(1);
-                }}
-              >
-                Home
-              </a>
-              <a
-                className="item"
-                onClick={() => {
-                  setOpen(0);
-                  onSelect(1);
-                }}
-              >
-                Über uns
-              </a>
-              <a
-                className="item"
-                onClick={() => {
-                  setOpen(0);
-                  onSelect(1);
-                }}
-              >
-                Kontakt
-              </a>
-            </nav>
-          )}
-        </div>
-        <div className="details">
-          <button
-            className={`border button extra circle ${selectedIcon !== 2 ? "large-elevate" : "small-elevate"}`}
-            onClick={() => setOpen(2)}
-          >
-            <img
-              className="responsive"
-              src="residential/residential-icon.png"
-            />
-          </button>
-          {open === 2 && (
-            <nav className="list">
-              <a
-                className="item"
-                onClick={() => {
-                  setOpen(0);
-                  onSelect(2);
-                }}
-              >
-                Home
-              </a>
-              <a
-                className="item"
-                onClick={() => {
-                  setOpen(0);
-                  onSelect(2);
-                }}
-              >
-                Über uns
-              </a>
-              <a
-                className="item"
-                onClick={() => {
-                  setOpen(0);
-                  onSelect(2);
-                }}
-              >
-                Kontakt
-              </a>
-            </nav>
-          )}
-        </div>
+        {Object.values(BuildingCategories).map(
+          (category) =>
+            category.buildable && (
+              <div key={category.name} className="details">
+                <button
+                  className={`border button extra circle ${selected && BuildingTypes[selected].category === category.name ? "large-elevate" : "small-elevate"}`}
+                  onClick={() => setOpen(category.name)}
+                >
+                  <img className="responsive" src={category.icon} />
+                </button>
+                {open === category.name && (
+                  <nav className="list">
+                    {Object.entries(BuildingTypes)
+                      .filter(
+                        ([, details]) => details.category === category.name,
+                      )
+                      .map(([type, details]) => (
+                        <button
+                          key={type}
+                          className={`border button extra circle ${selected && BuildingTypes[selected].category === category.name ? "large-elevate" : "small-elevate"}`}
+                          onClick={() => {
+                            setOpen(undefined);
+                            onSelect(type as BuildingType);
+                          }}
+                        >
+                          <img className="responsive" src={details.icon} />
+                        </button>
+                      ))}
+                  </nav>
+                )}
+              </div>
+            ),
+        )}
       </nav>
     </header>
   );
